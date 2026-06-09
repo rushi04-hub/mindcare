3e08e1acd3ee88d6efa634707f290335ff56e16a
Rushikesh <rmore8199@gmail.com>
2026-06-09 23:12:22 +0530

    Fix DB password variable

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWRD,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 17309
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
